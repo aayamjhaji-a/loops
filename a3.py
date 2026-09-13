cc7e1e088a3136a9c84137bafa561154027e84c7
aayamjhaji-a <aayamjhaji@gmail.com>
2026-09-13 20:19:31 +0545

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,9 @@
+# input number greater than 1
+n =int(input("enter the value of n:"))
+
+#print the number from n to 
+print("number from (0) to(1) are:".format(n,))
+
+#loop to print numbers
+for i in range(n,0,-1):
+    print(i)
